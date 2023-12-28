0e5299d8126d3f968296fbcc263fd13220c6e151
wh1t3h47 <tom.mharres@gmail.com>
2023-12-28 08:53:25 -0300

    feat(matplot): Improve plotting of ROC curve :hammer:

--- src/services/machine_learning_service.py
+++ src/services/machine_learning_service.py
@@ -1,19 +1,21 @@
+from typing import List, Dict
+from dataclasses import dataclass
+from matplotlib.pyplot import plot, figure, xlabel, ylabel, title, legend, show
+from pandas import json_normalize, read_json, DataFrame, Series, to_datetime
+from joblib import parallel_backend
+from os.path import abspath
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, precision_score, f1_score, roc_curve, auc
+from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold, cross_val_score, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold, cross_val_score
-from sklearn.metrics import accuracy_score, classification_report, precision_score, f1_score
-from sklearn.model_selection import train_test_split
-from joblib import parallel_backend
-from os.path import abspath
-import numpy as np
-import pandas as pd
-from pandas import to_datetime
 
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
-    transactions_list = df['transactions'].tolist()
-    df_normalized = pd.json_normalize(transactions_list)
+from src.models.TransactionModel import TransactionModel
+
+def preprocess_data(df: DataFrame) -> DataFrame:
+    transactions_list: List[TransactionModel] = df['transactions'].tolist()
+    df_normalized: DataFrame = json_normalize(transactions_list)
 
     df_normalized = df_normalized.drop(columns=['asset_description', 'asset_type'])
 
@@ -24,7 +26,8 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
 
     return df_normalized
 
-def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
+
+def feature_engineering(df: DataFrame) -> DataFrame:
     df['gain_avarage_in_1d'] = df['price_avarage_in_1d'].pct_change().apply(lambda x: 0 if abs(x) < 0.02 else x)
     df['gain_daily_after_1d'] = df['price_daily_after_1d'].pct_change().apply(lambda x: 0 if abs(x) < 0.02 else x)
 
@@ -35,24 +38,24 @@ def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
 
     df['toco_de_decisao'] = df['price_daily_after_15d'].apply(lambda x: x if x > 0 else 0)
 
-    # Adicionando colunas de variância para todos os preços
-    prices_columns = ['price_avarage_in_1d', 'price_avarage_in_7d', 'price_avarage_in_15d',
-                      'price_avarage_in_1m', 'price_avarage_in_6m', 'price_avarage_in_1y',
-                      'price_daily_after_1d', 'price_daily_after_7d', 'price_daily_after_15d',
-                      'price_daily_after_1m', 'price_daily_after_6m', 'price_daily_after_1y']
+    prices_columns: List[str] = ['price_avarage_in_1d', 'price_avarage_in_7d', 'price_avarage_in_15d',
+                                 'price_avarage_in_1m', 'price_avarage_in_6m', 'price_avarage_in_1y',
+                                 'price_daily_after_1d', 'price_daily_after_7d', 'price_daily_after_15d',
+                                 'price_daily_after_1m', 'price_daily_after_6m', 'price_daily_after_1y']
 
     for col in prices_columns:
         df[f'{col}_variance'] = df[col].pct_change().apply(lambda x: 0 if abs(x) < 0.02 else x)
 
     return df
 
-class ModelTrainer:
-    def __init__(self, model, name, param_dist):
-        self.model = model
-        self.name = name
-        self.param_dist = param_dist
 
-    def train(self, X_train, y_train, n_iter=50):
+@dataclass
+class ModelTrainer:
+    model: RandomForestClassifier
+    name: str
+    param_dist: Dict[str, List]
+
+    def train(self, X_train: DataFrame, y_train: Series, n_iter: int = 1) -> None:
         with parallel_backend('loky', n_jobs=10):
             random_search = RandomizedSearchCV(
                 self.model,
@@ -70,7 +73,7 @@ class ModelTrainer:
 
         self.model.fit(X_train, y_train)
 
-    def evaluate(self, X_test, y_test):
+    def evaluate(self, X_test: DataFrame, y_test: Series) -> None:
         if hasattr(self.model, 'predict_proba'):
             y_pred_proba = self.model.predict_proba(X_test)[:, 1]
             y_pred = (y_pred_proba > 0.4).astype(int)
@@ -93,16 +96,25 @@ class ModelTrainer:
         print(f"  - Classification Report:\n{report}")
         print(f"  - Cross-Validation Accuracy: {round(avg_cv_accuracy * 100):.0f}%")
 
+        # Plot ROC curve
+        if hasattr(self.model, 'predict_proba'):
+            fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+            roc_auc = auc(fpr, tpr)
+
+            plot(fpr, tpr, label=f'{self.name} (AUC = {roc_auc:.2f})')
+
+
 def machine_learning_service(file_path: str) -> None:
     abs_path = abspath(file_path)
-    df_denormalized = pd.read_json(abs_path)
+    df_denormalized = read_json(abs_path)
 
     df = preprocess_data(df_denormalized)
     df = feature_engineering(df)
 
-    feature_columns = ["gain_avarage_in_1d", "gain_daily_after_1d", "total_price_avarage", "total_price_daily_after", "price_daily_after_1y", "toco_de_decisao"]
+    feature_columns: List[str] = ["gain_avarage_in_1d", "gain_daily_after_1d", "total_price_avarage", "total_price_daily_after",
+                                  "price_daily_after_1y", "toco_de_decisao"]
 
-    missing_columns = set(feature_columns) - set(df.columns)
+    missing_columns: List[str] = list(set(feature_columns) - set(df.columns))
     if missing_columns:
         raise ValueError(f"Columns {missing_columns} are missing in the DataFrame.")
 
@@ -115,8 +127,7 @@ def machine_learning_service(file_path: str) -> None:
 
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
-    # Defina espaços de parâmetros adequados para o KNN
-    param_dist_knn = {
+    param_dist_knn: Dict[str, List] = {
         'n_neighbors': [5, 10, 15, 20, 25],
         'weights': ['uniform', 'distance'],
         'p': [1, 2]
@@ -124,42 +135,50 @@ def machine_learning_service(file_path: str) -> None:
 
     knn_model = ModelTrainer(KNeighborsClassifier(), "KNN", param_dist_knn)
     knn_model.train(X_train, y_train, n_iter=20)
-    knn_model.evaluate(X_test, y_test)
 
-    # Agora, os outros testes podem ser adicionados aqui
-    # Defina espaços de parâmetros adequados para os outros modelos
-    param_dist_rf = {
+    param_dist_rf: Dict[str, List] = {
         'n_estimators': [50, 100, 200, 300],
         'max_depth': [None, 10, 20, 30, 50],
         'min_samples_split': [2, 5, 10, 15],
         'min_samples_leaf': [1, 2, 4, 8]
     }
 
-    param_dist_dt = {
+    param_dist_dt: Dict[str, List] = {
         'criterion': ['gini', 'entropy'],
         'max_depth': [None, 10, 20, 30, 50],
         'min_samples_split': [2, 5, 10, 15],
         'min_samples_leaf': [1, 2, 4, 8]
     }
 
-    param_dist_nb = {}  # GaussianNB não tem muitos hiperparâmetros
+    param_dist_nb: Dict[str, List] = {}  # GaussianNB não tem muitos hiperparâmetros
 
     rf_model = ModelTrainer(RandomForestClassifier(random_state=42), "Random Forest", param_dist_rf)
     dt_model = ModelTrainer(DecisionTreeClassifier(random_state=42), "Decision Tree", param_dist_dt)
     nb_model = ModelTrainer(GaussianNB(), "GaussianNB", param_dist_nb)
 
     rf_model.train(X_train, y_train)
-    rf_model.evaluate(X_test, y_test)
-
     dt_model.train(X_train, y_train)
-    dt_model.evaluate(X_test, y_test)
-
     nb_model.train(X_train, y_train)
+
+    # Combine all ROC curves in one plot
+    figure()
+    plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--', label='Random')
+    knn_model.evaluate(X_test, y_test)
+    rf_model.evaluate(X_test, y_test)
+    dt_model.evaluate(X_test, y_test)
     nb_model.evaluate(X_test, y_test)
 
+    xlabel('False Positive Rate')
+    ylabel('True Positive Rate')
+    title('Receiver Operating Characteristic Curve')
+    legend()
+    show()
+
+
 def main() -> None:
-    file_path = "transactions.json"
+    file_path: str = "transactions.json"
     machine_learning_service(file_path)
+
 
 if __name__ == "__main__":
     main()
